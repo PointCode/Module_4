fae3f7bdca8a20308a6524459b66a275d98a769c
Mikle Brednew <mbr0969@gmail.com>
2016-01-24 01:58:56 +0300

    Выводим ФИО с помощью регулряного выражения

--- Name/src/NameRegular.java
+++ Name/src/NameRegular.java
@@ -1,5 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by papa on 24.01.2016.
  */
 public class NameRegular {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Введите фамилию имя и отчество: ");
+
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+
+        String[] words = str.split("[\\s*]");
+
+        System.out.println("Фамилия: " + words[0]);
+        System.out.println("Имя: " + words[1]);
+        System.out.println("Отчество: " + words[2]);
+
+    }
 }
