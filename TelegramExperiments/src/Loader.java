8ef0e687c54494a0ff73b3e5c3e84efa20300b0e
Mikle Brednew <mbr0969@gmail.com>
2016-01-24 03:37:08 +0300

    Исправленно форматирование

--- TelegramExperiments/src/Loader.java
+++ TelegramExperiments/src/Loader.java
@@ -16,30 +16,24 @@ public class Loader {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         TelegramApiBridge bridge = new TelegramApiBridge("149.154.167.50:443",24166,"1963c8d6a3afe0d9a75e676bcae679dd");
 
-
         System.out.println("PLEASE ENTER YOUR PHONE:  ");
         String phone = reader.readLine().trim();
 
 //// очищаем номер тлф от лишних символов
         phone = phone.replaceAll("[^0-9]+", "");
-
         AuthCheckedPhone authCheckedPhone = bridge.authCheckPhone(phone);
 
 //проверяем зарегистрирован номер или нет, если зарегистрирован, то авторизуемся
-        if (authCheckedPhone.isRegistered())
-        {
+        if (authCheckedPhone.isRegistered()){
+
             AuthSentCode authSentCode = bridge.authSendCode(phone);
             System.out.println(authSentCode);//выводим для отладки
-
             System.out.println("PLEASE ENTER SMS CODE: ");
             String smsCode = reader.readLine().trim();
-             AuthAuthorization authorization =  bridge.authSignIn(smsCode);
-
+            AuthAuthorization authorization =  bridge.authSignIn(smsCode);
             System.out.println("User name " + authorization.getUser());
 
-        }
-       else
-       {
+        }else{
           System.out.println("Your phone number is not registered!");
         }
 // разрываем соединение.
