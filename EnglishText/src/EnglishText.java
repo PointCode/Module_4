dcfc7ed5c96c8df5a53361a1b89efa6a2c987872
Mikle Brednew <mbr0969@gmail.com>
2016-01-24 14:28:45 +0300

    Английский текст

--- EnglishText/src/EnglishText.java
+++ EnglishText/src/EnglishText.java
@@ -4,9 +4,23 @@
 public class EnglishText {
     public static void main(String[] args) {
 
-String str = "The quality of implementation specifications concern two properties, accuracy of the returned result and monotonicity of the method. Accuracy of the floating-point Math methods is measured in terms of ulps, units in the last place. For a given floating-point format, an ulp of a specific real number value is the distance between the two floating-point values bracketing that numerical value. When discussing the accuracy of a method as a whole rather than at a specific argument, the number of ulps cited is for the worst-case error at any argument. If a method always has an error less than 0.5 ulps, the method always returns the floating-point number nearest the exact result; such a method is correctly rounded. A correctly rounded method is generally the best a floating-point approximation can be; however, it is impractical for many floating-point methods to be correctly rounded. Instead, for the Math class, a larger error bound of 1 or 2 ulps is allowed for certain methods. Informally, with a 1 ulp error bound, when the exact result is a representable number, the exact result should be returned as the computed result; otherwise, either of the two floating-point values which bracket the exact result may be returned.";
+String str = "The quality of implementation specifications concern two properties, " +
+        "accuracy of the returned result and monotonicity of the method. Accuracy of the " +
+        "floating-point Math methods is measured in terms of ulps, units in the last place. " +
+        "For a given floating-point format, an ulp of a specific real number value is the distance " +
+        "between the two floating-point values bracketing that numerical value. " +
+        "When discussing the accuracy of a method as a whole rather than at a specific argument, " +
+        "the number of ulps cited is for the worst-case error at any argument. If a method always " +
+        "has an error less than 0.5 ulps, the method always returns the floating-point number " +
+        "nearest the exact result; such a method is correctly rounded. A correctly rounded method is " +
+        "generally the best a floating-point approximation can be; however, it is impractical for " +
+        "many floating-point methods to be correctly rounded. Instead, for the Math class, a larger " +
+        "error bound of 1 or 2 ulps is allowed for certain methods. Informally, with a 1 ulp error bound, " +
+        "when the exact result is a representable number, the exact result should be returned as the " +
+        "computed result; otherwise, either of the two floating-point values which bracket the exact " +
+        "result may be returned.";
 
-        String[] words = str.split("[\\p{Punct}\\s*]");
+        String[] words = str.split("\\s*[^a-zA-Z0-9]");
 
         for (String word: words){
 
