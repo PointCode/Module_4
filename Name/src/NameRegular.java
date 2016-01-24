5b5dcc5c9e3db046f824b73ed9ee065bc2fd33c5
Mikle Brednew <mbr0969@gmail.com>
2016-01-24 03:37:08 +0300

    ДОбавленн вывод ФИО с помощью регулярки

--- Name/src/NameRegular.java
+++ Name/src/NameRegular.java
@@ -2,9 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Created by papa on 24.01.2016.
- */
 public class NameRegular {
     public static void main(String[] args) throws IOException {
 
